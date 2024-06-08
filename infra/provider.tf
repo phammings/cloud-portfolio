73811b8b351253caf5450a53c3d48065c5bc5ad2
Ryan Pham <ryanlepham9@gmail.com>
2024-06-08 01:36:44 -0400

    Updated backend cicd yml

--- infra/provider.tf
+++ infra/provider.tf
@@ -1,12 +1,13 @@
 terraform {
   required_providers {
     aws = {
-      version = ">=4.9.0"
+      version = "3.74.0"
       source  = "hashicorp/aws"
     }
   }
 }
 
 provider "aws" {
+  profile = "default"
   region  = "ca-central-1"
 }
